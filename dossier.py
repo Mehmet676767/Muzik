232f2007acb5bf6ad2c66e2222131fca580392fa
Mehmet676767 <162468939+Mehmet676767@users.noreply.github.com>
2024-06-06 16:17:27 +0300

    Add files via upload

---
+++ dossier.py
@@ -0,0 +1,82 @@
+# MIT License
+#
+# Copyright (c) 2023 AnonymousX1025
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from FallenMusic import BOT_NAME
+
+PM_START_TEXT = """
+M𝙚𝙧𝙝𝙖𝙗𝙖 {0}, 🥀
+𝙗𝙚𝙣 ** {1} !
+
+🇹🇷𝙩𝙪̈𝙧𝙠𝙞𝙮𝙚𝙣𝙞𝙣 𝙚𝙣 𝙠𝙖𝙡𝙞𝙩𝙚𝙡𝙞 𝙢𝙪̈𝙯𝙞𝙠 𝙗𝙤𝙩𝙪.
+"""
+
+START_TEXT = """
+**merhaba** {0}, 🥀
+  {1} 𝙜𝙧𝙪𝙥𝙩𝙖 𝙢𝙪̈𝙯𝙞𝙠 𝙙𝙞𝙣𝙡𝙚𝙤̈𝙚𝙠 𝙞𝙘̧𝙞𝙣 𝙜𝙚𝙡𝙞𝙨̧𝙩𝙞𝙧𝙞𝙡𝙙𝙞𝙤̈ {2}.
+
+──────────────────
+🇹🇷 𝙙𝙚𝙨𝙩𝙚𝙠 𝙜𝙧𝙪𝙗𝙪𝙤̈𝙪𝙯 [𝙤̈𝙚𝙧𝙫𝙚 𝙙𝙚𝙨𝙩𝙚𝙠]({3}).
+"""
+
+HELP_TEXT = f"""
+<u>❄ **𝙗𝙪𝙣𝙡𝙖𝙧 𝙗𝙚𝙣𝙞𝙢 𝙠𝙤𝙢𝙪𝙩𝙡𝙖𝙧𝜾𝙢 {BOT_NAME} :**</u>
+
+ /oynat : şarkı başlatır.
+ /voynat : video şarkı başlatır.
+ /dur : şarki durdurur.
+ /devam : şarkıyı calmaya devam eder.
+ /atla : sıradaki parçaya atlatır.
+ /son : şarkıyı kapatır.
+
+ /ping : sistemin son durumunu gösterir.
+ /sudolist : yönetici listesini gösterir.
+
+ /bul : şarkıyı arar bulur indirir.
+
+ /ara : bilgi arar.
+"""
+
+HELP_SUDO = f"""
+<u>✨ **sᴜᴅᴏ ᴄᴏᴍᴍᴀɴᴅs ɪɴ {BOT_NAME} :**</u>
+
+ /activevc : aktiviteleri gösterir.
+ /eval or /sh : ʀᴜɴs ᴛʜᴇ ɢɪᴠᴇɴ ᴄᴏᴅᴇ ᴏɴ ᴛʜᴇ ʙᴏᴛ's ᴛᴇʀᴍɪɴᴀʟ.
+ /speedtest : ʀᴜɴs ᴀ sᴘᴇᴇᴅᴛᴇsᴛ ᴏɴ ᴛʜᴇ ʙᴏᴛ's sᴇʀᴠᴇʀ.
+ /sysstats : sʜᴏᴡs ᴛʜᴇ sʏsᴛᴇᴍ sᴛᴀᴛs ᴏғ ᴛʜᴇ ʙᴏᴛ's sᴇʀᴠᴇʀ.
+
+ /setname [ᴛᴇxᴛ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ] : ᴄʜᴀɴɢᴇ ᴛʜᴇ ᴀssɪsᴛᴀɴᴛ ᴀᴄᴄᴏᴜɴᴛ ɴᴀᴍᴇ.
+ /setbio [ᴛᴇxᴛ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ] : ᴄʜᴀɴɢᴇ ᴛʜᴇ ʙɪᴏ ᴏғ ᴛʜᴇ ᴀssɪsᴛᴀɴᴛ ᴀᴄᴄᴏᴜɴᴛ.
+ /setpfp [ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴘʜᴏᴛᴏ] : ᴄʜᴀɴɢᴇ ᴛʜᴇ ᴘғᴘ ᴏғ ᴛʜᴇ ᴀssɪsᴛᴀɴᴛ ᴀᴄᴄᴏᴜɴᴛ.
+ /delpfp : ᴅᴇʟᴇᴛᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴘғᴘ ᴏғ ᴛʜᴇ ᴀssɪsᴛᴀɴᴛ ᴀᴄᴄᴏᴜɴᴛ.
+"""
+
+HELP_DEV = f"""
+<u>✨ **ᴏᴡɴᴇʀ ᴄᴏᴍᴍᴀɴᴅs ɪɴ {BOT_NAME} :**</u>
+
+ /config : ᴛᴏ ɢᴇᴛ ᴀʟʟ ᴄᴏɴꜰɪɢ ᴠᴀʀɪᴀʙʟᴇꜱ ᴏꜰ ʙᴏᴛ.
+ /broadcast [ᴍᴇssᴀɢᴇ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ] : ʙʀᴏᴀᴅᴄᴀsᴛ ᴛʜᴇ ᴍᴇssᴀɢᴇ ᴛᴏ sᴇʀᴠᴇᴅ ᴄʜᴀᴛs ᴏғ ᴛʜᴇ ʙᴏᴛ.
+ /rmdownloads : ᴄʟᴇᴀʀs ᴛʜᴇ ᴄᴀᴄʜᴇ ғɪʟᴇs ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ᴏɴ ᴛʜᴇ ʙᴏᴛ's sᴇʀᴠᴇʀ.
+ /leaveall : ᴏʀᴅᴇʀs ᴛʜᴇ ᴀssɪsᴛᴀɴᴛ ᴀᴄᴄᴏᴜɴᴛ ᴛᴏ ʟᴇᴀᴠᴇ ᴀʟʟ ᴄʜᴀᴛs.
+
+๏ /addsudo [ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜsᴇʀ] : ᴀᴅᴅ ᴛʜᴇ ᴜsᴇʀ ᴛᴏ sᴜᴅᴏ ᴜsᴇʀs ʟɪsᴛ.
+๏ /rmsudo [ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜsᴇʀ] : ʀᴇᴍᴏᴠᴇ ᴛʜᴇ ᴜsᴇʀ ғʀᴏᴍ sᴜᴅᴏ ᴜsᴇʀs ʟɪsᴛ.
+"""
---
+++ play.py
@@ -0,0 +1,258 @@
+# MIT License
+#
+# Copyright (c) 2023 AnonymousX1025
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import asyncio
+import os
+
+from pyrogram import filters
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.errors import (
+    ChatAdminRequired,
+    UserAlreadyParticipant,
+    UserNotParticipant,
+)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from pytgcalls import StreamType
+from pytgcalls.exceptions import NoActiveGroupCall, TelegramServerError, UnMuteNeeded
+from pytgcalls.types import AudioPiped, HighQualityAudio
+from youtube_search import YoutubeSearch
+
+from config import DURATION_LIMIT
+from FallenMusic import (
+    ASS_ID,
+    ASS_MENTION,
+    ASS_NAME,
+    ASS_USERNAME,
+    BOT_NAME,
+    BOT_USERNAME,
+    LOGGER,
+    app,
+    app2,
+    fallendb,
+    pytgcalls,
+)
+from FallenMusic.Helpers.active import add_active_chat, is_active_chat, stream_on
+from FallenMusic.Helpers.downloaders import audio_dl
+from FallenMusic.Helpers.errors import DurationLimitError
+from FallenMusic.Helpers.gets import get_file_name, get_url
+from FallenMusic.Helpers.inline import buttons
+from FallenMusic.Helpers.queue import put
+from FallenMusic.Helpers.thumbnails import gen_qthumb, gen_thumb
+
+
+@app.on_message(
+    filters.command(["play", "voynat", "oynat"])
+    & filters.group
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+async def play(_, message: Message):
+    fallen = await message.reply_text("» 𝚜̧𝚊𝚛𝚔ｪ 𝚒𝚜̧𝚕𝚎𝚗𝚒𝚢𝚘𝚛 𝚕𝚞̈𝚝𝚏𝚎𝚗 𝚋𝚎𝚔𝚕𝚎𝚢𝚒𝚗..")
+    try:
+        await message.delete()
+    except:
+        pass
+
+    try:
+        try:
+            get = await app.get_chat_member(message.chat.id, ASS_ID)
+        except ChatAdminRequired:
+            return await fallen.edit_text(
+                f"» 𝚔𝚞𝚜̧𝚜̧𝚊𝚗ｪ𝚌ｪ 𝚍𝚊𝚟𝚎𝚝 𝚎𝚝𝚖𝚎 𝚋𝚊𝚐̆𝚕𝚊𝚗𝚝ｪ𝚜ｪ 𝚢𝚘𝚕𝚞𝚢𝚕𝚊 𝚍𝚊𝚟𝚎𝚝 𝚎𝚝𝚖𝚎 𝚒𝚣𝚗𝚒𝚖 𝚢𝚘𝚔 {BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛᴏ {message.chat.title}."
+            )
+        if get.status == ChatMemberStatus.BANNED:
+            unban_butt = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text=f"ᴜɴʙᴀɴ {ASS_NAME}",
+                            callback_data=f"unban_assistant {message.chat.id}|{ASS_ID}",
+                        ),
+                    ]
+                ]
+            )
+            return await fallen.edit_text(
+                text=f"» {BOT_NAME} asistan yasaklandı {message.chat.title}\n\n𖢵 ɪᴅ : `{ASS_ID}`\n𖢵 ismi : {ASS_MENTION}\n𖢵 kullanıcı adı: @{ASS_USERNAME}\n\n...",
+                reply_markup=unban_butt,
+            )
+    except UserNotParticipant:
+        if message.chat.username:
+            invitelink = message.chat.username
+            try:
+                await app2.resolve_peer(invitelink)
+            except Exception as ex:
+                LOGGER.error(ex)
+        else:
+            try:
+                invitelink = await app.export_chat_invite_link(message.chat.id)
+            except ChatAdminRequired:
+                return await fallen.edit_text(
+                    f"» kullanıcıları davet etme bağlantısı yoluyla davet etme iznim yok {BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛᴏ {message.chat.title}."
+                )
+            except Exception as ex:
+                return await fallen.edit_text(
+                    f"davet edilemedi {BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛᴏ {message.chat.title}.\n\n**sebep :** `{ex}`"
+                )
+        if invitelink.startswith("https://t.me/+"):
+            invitelink = invitelink.replace("https://t.me/+", "https://t.me/joinchat/")
+        anon = await fallen.edit_text(
+            f"yükleniyor...\n\ndavetkar {ASS_NAME} bu {message.chat.title}."
+        )
+        try:
+            await app2.join_chat(invitelink)
+            await asyncio.sleep(2)
+            await fallen.edit_text(
+                f"{ASS_NAME} başarıyla katıldı,\n\n yayın başladı..."
+            )
+        except UserAlreadyParticipant:
+            pass
+        except Exception as ex:
+            return await fallen.edit_text(
+                f"davet edilmedi {BOT_NAME} ᴀssɪsᴛᴀɴᴛ ᴛᴏ {message.chat.title}.\n\n**ʀᴇᴀsᴏɴ :** `{ex}`"
+            )
+        try:
+            await app2.resolve_peer(invitelink)
+        except:
+            pass
+
+    ruser = message.from_user.first_name
+    audio = (
+        (message.reply_to_message.audio or message.reply_to_message.voice)
+        if message.reply_to_message
+        else None
+    )
+    url = get_url(message)
+    if audio:
+        if round(audio.duration / 60) > DURATION_LIMIT:
+            raise DurationLimitError(
+                f"» kusura bakma bebeği, daha uzun takip ediyorum  {DURATION_LIMIT} dakikaların oynanmasına izin verilmiyor {BOT_NAME}."
+            )
+
+        file_name = get_file_name(audio)
+        title = file_name
+        duration = round(audio.duration / 60)
+        file_path = (
+            await message.reply_to_message.download(file_name)
+            if not os.path.isfile(os.path.join("downloads", file_name))
+            else f"downloads/{file_name}"
+        )
+
+    elif url:
+        try:
+            results = YoutubeSearch(url, max_results=1).to_dict()
+            title = results[0]["title"]
+            duration = results[0]["duration"]
+            videoid = results[0]["id"]
+
+            secmul, dur, dur_arr = 1, 0, duration.split(":")
+            for i in range(len(dur_arr) - 1, -1, -1):
+                dur += int(dur_arr[i]) * secmul
+                secmul *= 60
+
+        except Exception as e:
+            return await fallen.edit_text(f"»davet edilmedi\n\n**asistan :** `{e}`")
+
+        if (dur / 60) > DURATION_LIMIT:
+            return await fallen.edit_text(
+                f"» Üzgünüm bebeğim, daha uzun süre takip et  {DURATION_LIMIT} dakikaların oynanmasına izin verilmiyor {BOT_NAME}."
+            )
+        file_path = audio_dl(url)
+    else:
+        if len(message.command) <2:
+            return await fallen.edit_text("» ne oynatmak istersin bebeğim örnek /oynat taladro ?")
+        await fallen.edit_text("🎤")
+        query = message.text.split(None, 1)[1]
+        try:
+            results = YoutubeSearch(query, max_results=1).to_dict()
+            url = f"https://youtube.com{results[0]['url_suffix']}"
+            title = results[0]["title"]
+            videoid = results[0]["id"]
+            duration = results[0]["duration"]
+
+            secmul, dur, dur_arr = 1, 0, duration.split(":")
+            for i in range(len(dur_arr) - 1, -1, -1):
+                dur += int(dur_arr[i]) * secmul
+                secmul *= 60
+
+        except Exception as e:
+            LOGGER.error(str(e))
+            return await fallen.edit("» şarkıyı bulamadım aşkım başka şarkı açmayı dene...")
+
+        if (dur / 60) > DURATION_LIMIT:
+            return await fallen.edit(
+                f"»Üzgünüm bebeğim, daha uzun süre takip et {DURATION_LIMIT} dakikaların oynanmasına izin verilmiyor {BOT_NAME}."
+            )
+        file_path = audio_dl(url)
+
+    try:
+        videoid = videoid
+    except:
+        videoid = "fuckitstgaudio"
+    if await is_active_chat(message.chat.id):
+        await put(
+            message.chat.id,
+            title,
+            duration,
+            videoid,
+            file_path,
+            ruser,
+            message.from_user.id,
+        )
+        position = len(fallendb.get(message.chat.id))
+        qimg = await gen_qthumb(videoid, message.from_user.id)
+        await message.reply_photo(
+            photo=qimg,
+            caption=f"*şarkı sıraya eklendi {position}**\n\n‣ **bilgi :** [{title[:27]}](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n‣ **süre :** `{duration}` dakika\n‣ **istek sahibi :** {ruser}",
+            reply_markup=buttons,
+        )
+    else:
+        stream = AudioPiped(file_path, audio_parameters=HighQualityAudio())
+        try:
+            await pytgcalls.join_group_call(
+                message.chat.id,
+                stream,
+                stream_type=StreamType().pulse_stream,
+            )
+
+        except NoActiveGroupCall:
+            return await fallen.edit_text(
+                "**» sesli sohbet gurubu kapalı.**\n\nlutfen görüntülü sohbet grubunu açın ."
+            )
+        except TelegramServerError:
+            return await fallen.edit_text(
+                "» Telegram'da bazı dahili sorunlar yaşanıyor. Lütfen görüntülü sohbeti yeniden başlatıp tekrar deneyin.."
+            )
+        except UnMuteNeeded:
+            return await fallen.edit_text(
+                f"» {BOT_NAME} Asistanın görüntülü sohbette sesi kapatıldı,\n\nᴘʟᴇᴀsᴇ ᴜɴᴍᴜᴛᴇ {ASS_MENTION} ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴛʀʏ ᴘʟᴀʏɪɴɢ ᴀɢᴀɪɴ."
+            )
+
+        imgt = await gen_thumb(videoid, message.from_user.id)
+        await stream_on(message.chat.id)
+        await add_active_chat(message.chat.id)
+        await message.reply_photo(
+            photo=imgt,
+            caption=f"**şarkı başladı**\n\n‣ **bilgi :** [{title[:27]}](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n‣ **süre :** `{duration}` dakika\n‣ **talep eden:** {ruser}",
+            reply_markup=buttons,
+        )
+
+    return await fallen.delete()
